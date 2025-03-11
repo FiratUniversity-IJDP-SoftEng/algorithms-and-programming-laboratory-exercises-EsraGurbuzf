fa8a48c28966ed7210b29317eb3094c9ada055d6
Esra Gurbuz <esragurbuze@gmail.com>
2025-03-11 14:42:11 +0300

    ex006.py is completed

--- Exercises/ex006.py
+++ Exercises/ex006.py
@@ -1,10 +1,22 @@
 # Your Student ID: 230543001
 # Your Name and Surname: Esra Gürbüz
 
-print("Original list:")
-for x in range(1,11):
-    print(x, end=" ")
+original_list = list(range(1, 11))
+print(original_list)
 
-print("\nReverse list:")
-for x in range(10,0,-1):
-    print(x, end=" ")
+reverse_list = original_list[::-1]
+print(reverse_list)
+
+original_list = original_list + [11] + [12] + [13]
+print(original_list)
+
+length_of_original_list = len(original_list)
+print(int(length_of_original_list))
+
+original_list.remove(original_list[0])
+original_list.remove(original_list[length_of_original_list-2]) #-2 because we removed 0
+print(original_list)
+
+original_list = list(range(1, 11))
+even_numbers_list = original_list[1::2]
+print(even_numbers_list)
