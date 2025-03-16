f60bb31913372a6b529d6c1d57573c076a2d20b0
Esra Gurbuz <esragurbuze@gmail.com>
2025-03-16 07:27:25 +0300

    Ex007 to Ex010 was completed.

--- Exercises/ex007.py
+++ Exercises/ex007.py
@@ -1,2 +1,17 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID: 230543001
+# Your Name and Surname: Esra Gürbüz
+
+word = input("Enter a string: ")
+
+dictionary = {}
+
+for char in word:
+    if char in dictionary:
+        dictionary[char] += 1
+    else:
+        dictionary[char] = 1
+
+for char in sorted(dictionary):
+    print(f"{char} : {dictionary[char]}")
+
+
--- Exercises/ex008.py
+++ Exercises/ex008.py
@@ -1,2 +1,6 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID: 230543001
+# Your Name and Surname: Esra Gürbüz
+
+rows = int(input("Enter a number: "))
+for i in range(rows):
+    print( " " * (rows - i - 1) + "*" * (2 * i + 1))
